5d4a38d4b32ff312704ed283f3237157ad500863
nastark123 <vikingsiowa28@gmail.com>
2020-12-05 20:50:52 -0600

    Added colors and fixed bug

--- src/cntl.c
+++ src/cntl.c
@@ -3,12 +3,12 @@
 int display_output(char *out, int len, int out_mode) {
     switch(out_mode) {
         case MODE_ASCII:
-            printf("\nRead %d bytes from device\n", len);
-            printf("Data: %s\n", out);
+            printf(ANSI_COLOR_GREEN "\nRead %d bytes from device\n", len);
+            printf("Data: " ANSI_COLOR_RESET "%s\n", out);
             break;
 
         case MODE_HEX:
-            printf("\nRead %d bytes from device:\n", len);
+            printf(ANSI_COLOR_GREEN "\nRead %d bytes from device:\n" ANSI_COLOR_RESET, len);
             for(int i = 0; i < len; i++) {
                 printf("%x ", out[i]);
             }
@@ -16,23 +16,23 @@ int display_output(char *out, int len, int out_mode) {
             break;
 
         case MODE_FILE:
-            printf("\nInput a filename to write to:\n");
+            printf(ANSI_COLOR_GREEN "\nInput a filename to write to:\n" ANSI_COLOR_RESET);
             char *file = malloc(256);
             fgets(file, 256, stdin);
             file[strlen(file) - 1] = '\0';
 
             int f = open(file, O_RDWR);
-            if(f < 0) printf("Error opening file\n");
+            if(f < 0) printf(ANSI_COLOR_RED "Error opening file\n" ANSI_COLOR_RESET);
             write(f, out, len);
             close(f);
                     
-            printf("\nWrote %d bytes to file %s\n", len, file);
+            printf(ANSI_COLOR_GREEN "\nWrote %d bytes to file %s\n" ANSI_COLOR_RESET, len, file);
             free(file);
             break;
 
         default:
-            printf("\nRead %d bytes from device\n", len);
-            printf("Data: %s\n", out);
+            printf(ANSI_COLOR_GREEN "\nRead %d bytes from device\n", len);
+            printf("Data: " ANSI_COLOR_RESET "%s\n", out);
             break;
     }
 }
@@ -69,16 +69,19 @@ int disconnect(serial_dev *dev) {
 }
 
 int ch_dev(serial_dev *dev, char *cmd) {
-    int r = disconnect(dev);
-    if (r < 0) return -1;
-    dev->dev = realloc(dev->dev, strlen(cmd) - 8);
+    if(dev->fd > 0) {
+        int r = disconnect(dev);
+        if (r < 0) return -1;
+    }
+    dev->dev = realloc(dev->dev, strlen(cmd) - 6);
+    memset(dev->dev, 0, strlen(cmd) - 6);
 
     // we go up to strlen - 1 to avoid getting the newline character, which can cause issues
     for(int i = 7; i < strlen(cmd) - 1; i++) {
         dev->dev[i - 7] = cmd[i];
     }
 
-    printf("\nConnecting to %s\n", dev->dev);
+    printf(ANSI_COLOR_GREEN "\nConnecting to %s\n" ANSI_COLOR_RESET, dev->dev);
 
     dev->fd = initialize_serial(dev);
     if(dev->fd < 0) return -1;
@@ -97,7 +100,7 @@ int ch_baud(serial_dev *dev, char *cmd) {
     }
     dev->baud = strtol(temp, NULL, 10);
 
-    printf("\nSetting baudrate to %d...\n", dev->baud);
+    printf(ANSI_COLOR_GREEN "\nSetting baudrate to %d...\n" ANSI_COLOR_RESET, dev->baud);
 
     dev->baud = parse_baud(dev->baud);
 
@@ -111,7 +114,7 @@ int ch_baud(serial_dev *dev, char *cmd) {
 }
 
 void ls_cmd(){
-    printf("\nWelcome to SerialCom!  All commands are preceded by a \"!\" and are as follows:\n");
+    printf(ANSI_COLOR_GREEN "\nWelcome to SerialCom!  All commands are preceded by a \"!\" and are as follows:\n");
     printf("dc - disconnect from the current serial device\n");
     printf("chdev - change the device you are currently connected to\n");
     printf("chbd - change the baud rate of the serial connection\n");
@@ -119,27 +122,27 @@ void ls_cmd(){
     printf("omode - change the mode to one of the supported input modes\n");
     printf("lscmd - list all commands\n");
     printf("lsbd - list all the supported baud rates compiled into this executable\n");
-    printf("lsmode - lists all supported modes (ascii, hex, etc.)\n");
+    printf("lsmode - lists all supported modes (ascii, hex, etc.)\n" ANSI_COLOR_RESET);
 }
 
 void ls_bd() {
-    printf("\nThe supported baud rates in this executable are:\n");
+    printf(ANSI_COLOR_GREEN "\nThe supported baud rates in this executable are:\n");
     printf("50, 75, 110, 134.5, 150, 200, 300, 600, 1200\n");
-    printf("1800, 2400, 4800, 9600, 19200, 38400\n");
+    printf("1800, 2400, 4800, 9600, 19200, 38400\n" ANSI_COLOR_RESET);
     #ifdef __USE_MISC
-    printf("\nExtended baud rates available are:\n");
+    printf(ANSI_COLOR_GREEN "\nExtended baud rates available are:\n");
     printf("57600, 115200, 230400, 460800, 500000\n");
     printf("576000, 921600, 1000000, 1152000\n");
     printf("1500000, 2000000, 2500000, 3000000\n");
-    printf("3500000, 4000000\n");
+    printf("3500000, 4000000\n" ANSI_COLOR_RESET);
     #endif
 }
 
 void ls_mode() {
-    printf("\nCurrently supported modes:\n");
+    printf(ANSI_COLOR_GREEN "\nCurrently supported modes:\n");
     printf("ascii - interpret characters as ascii (default)\n");
     printf("hex - translate characters as space delimited hexadecimal\n");
-    printf("file - reads data from a file and writes it to the serial device\n");
+    printf("file - reads data from a file and writes it to the serial device\n" ANSI_COLOR_RESET);
 }
 
 int ch_mode(char *cmd) {
@@ -151,15 +154,15 @@ int ch_mode(char *cmd) {
 
     if(!strncmp(mode, "ascii", 5)) {
         free(mode);
-        printf("\nNow in ASCII mode\n");
+        printf(ANSI_COLOR_GREEN "\nNow in ASCII mode\n" ANSI_COLOR_RESET);
         return MODE_ASCII;
     } else if(!strncmp(mode, "hex", 3)) {
         free(mode);
-        printf("\nNow in HEX mode\n");
+        printf(ANSI_COLOR_GREEN "\nNow in HEX mode\n" ANSI_COLOR_RESET);
         return MODE_HEX;
     } else if(!strncmp(mode, "file", 4)) {
         free(mode);
-        printf("\nNow in file mode\n");
+        printf(ANSI_COLOR_GREEN "\nNow in file mode\n" ANSI_COLOR_RESET);
         return MODE_FILE;
     }
 
@@ -169,17 +172,17 @@ int ch_mode(char *cmd) {
 int init_mode(char *cmd) {
 
     if(!strncmp(cmd, "ascii", 5)) {
-        printf("\nSet to ASCII mode\n");
+        printf(ANSI_COLOR_GREEN "\nSet to ASCII mode\n" ANSI_COLOR_RESET);
         return MODE_ASCII;
     } else if(!strncmp(cmd, "hex", 3)) {
-        printf("\nSet to HEX mode\n");
+        printf(ANSI_COLOR_GREEN "\nSet to HEX mode\n" ANSI_COLOR_RESET);
         return MODE_HEX;
     } else if(!strncmp(cmd, "file", 4)) {
-        printf("\nSet to file mode\n");
+        printf(ANSI_COLOR_GREEN "\nSet to file mode\n" ANSI_COLOR_RESET);
         return MODE_FILE;
     }
 
-    printf("\nUnrecognized mode, starting in ASCII\n");
+    printf(ANSI_COLOR_RED "\nUnrecognized mode, starting in ASCII\n" ANSI_COLOR_RESET);
     return MODE_ASCII;
 }
 
--- src/main.c
+++ src/main.c
@@ -14,30 +14,30 @@ int main(int argc, char *argv[]) {
     serial_dev *dev;
     dev = malloc(sizeof(serial_dev));
 
-    printf("Welcome to SerialCom!  What device would you like to connect to?\n");
+    printf(ANSI_COLOR_GREEN "Welcome to SerialCom!  What device would you like to connect to?\n" ANSI_COLOR_RESET);
     fgets(setup, 256, stdin);
     setup[strlen(setup) - 1] = '\0';
     dev->dev = malloc(strlen(setup));
     strcpy(dev->dev, setup);
 
-    printf("\nWhat baud rate would you like to use?\n");
+    printf(ANSI_COLOR_GREEN "\nWhat baud rate would you like to use?\n" ANSI_COLOR_RESET);
     memset(setup, 0, 256);
     fgets(setup, 256, stdin);
     long baud = strtol(argv[2], NULL, 10);
     baud = parse_baud(baud);
     dev->baud = baud;
 
-    printf("\nWhat would you like as your starting input mode (ascii, hex, or file)?\n");
+    printf(ANSI_COLOR_GREEN "\nWhat would you like as your starting input mode (ascii, hex, or file)?\n" ANSI_COLOR_RESET);
     memset(setup, 0, 256);
     fgets(setup, 256, stdin);
     dev->in_mode = init_mode(setup);
 
-    printf("\nWhat would you like as your starting output mode (ascii, hex, or file)?\n");
+    printf(ANSI_COLOR_GREEN "\nWhat would you like as your starting output mode (ascii, hex, or file)?\n" ANSI_COLOR_RESET);
     memset(setup, 0, 256);
     fgets(setup, 256, stdin);
     dev->out_mode = init_mode(setup);
 
-    printf("\nWhat would you like the timeout to be (in 0.1's of a second):\n");
+    printf(ANSI_COLOR_GREEN "\nWhat would you like the timeout to be (in 0.1's of a second):\n" ANSI_COLOR_RESET);
     memset(setup, 0, 256);
     fgets(setup, 256, stdin);
     dev->timeout = strtol(setup, NULL, 10);
@@ -47,17 +47,17 @@ int main(int argc, char *argv[]) {
     initialize_serial(dev);
 
     if(dev->fd < 0) {
-        printf("Error %d while initializing: ", errno);
-        printf("%s\n", strerror(errno));
+        printf(ANSI_COLOR_RED "Error %d while initializing: " ANSI_COLOR_RESET, errno);
+        printf(ANSI_COLOR_RED "%s\n", strerror(errno));
     } else {
-        printf("Device opened and initialized\n");
+        printf(ANSI_COLOR_GREEN "Device opened and initialized\n" ANSI_COLOR_RESET);
     }
     char *buff = malloc(256);
     
     for(;;) {
         memset(buff, 0, 256);
 
-        printf("\nInput a string to send to the device (max 255 characters) or input a command:\n");
+        printf(ANSI_COLOR_GREEN "\nInput a string to send to the device (max 255 characters) or input a command:\n" ANSI_COLOR_RESET);
         fgets(buff, 256, stdin);
 
         int n = 0;
@@ -71,7 +71,7 @@ int main(int argc, char *argv[]) {
                     n = parse_hex(buff);
                 } else if(dev->in_mode == MODE_FILE) {
                     int f = open(buff, O_RDONLY);
-                    if(f < 0) printf("\nError opening file\n");
+                    if(f < 0) printf(ANSI_COLOR_RED "\nError opening file\n" ANSI_COLOR_RESET);
                     n = read(f, buff, 256);
                     close(f);
                 } else {
@@ -90,24 +90,24 @@ int main(int argc, char *argv[]) {
             case DISCONNECT:
                 n = disconnect(dev);
                 if (n < 0) {
-                    printf("\nError %d: ", errno);
-                    printf("%s\n", strerror(errno));
+                    printf(ANSI_COLOR_RED "\nError %d: " ANSI_COLOR_RESET, errno);
+                    printf(ANSI_COLOR_RED "%s\n" ANSI_COLOR_RESET, strerror(errno));
                 }
                 break;
 
             case CHDEVICE:
                 n = ch_dev(dev, buff);
                 if (n < 0) {
-                    printf("\nError %d: ", errno);
-                    printf("%s\n", strerror(errno));
+                    printf(ANSI_COLOR_RED "\nError %d: " ANSI_COLOR_RESET, errno);
+                    printf(ANSI_COLOR_RED "%s\n" ANSI_COLOR_RESET, strerror(errno));
                 }
                 break;
 
             case CHBAUD:
                 n = ch_baud(dev, buff);
                 if (n < 0) {
-                    printf("\nError %d: ", errno);
-                    printf("%s\n", strerror(errno));
+                    printf(ANSI_COLOR_RED "\nError %d: " ANSI_COLOR_RESET, errno);
+                    printf(ANSI_COLOR_RED "%s\n" ANSI_COLOR_RESET, strerror(errno));
                 }
                 break;
 
